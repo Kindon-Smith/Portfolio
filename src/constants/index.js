1e64b19551087e13dce48c8817e5e4a629c3d31b
lim3r <kindon@bluelim3.com>
2023-12-01 21:13:12 -0800

    Reversion of constants file

--- src/constants/index.js
+++ src/constants/index.js
@@ -120,6 +120,67 @@ import {
   ];
   
   const experiences = [
+    {
+      title: "Developer Support Engineer",
+      company_name: "Accenture",
+      icon: firebase,
+      iconBg: "#383E56",
+      date: "Oct 2022 - Present",
+      points: [
+        "Oversee app developer issues with a focus on Firebase Cloud Messaging (FCM) and Google Analytics (GA) for Firebase.",
+        "Build developer trust by resolving problems with FCM delivery through SDK and Rest API calls.",
+        "Educate developers on GA implementation, data collection, and interpretation.",
+        "Currently hold certifications as a Subject Matter Expert in FCM and GA for Firebase",
+      ],
+    },
+    {
+      title: "Cloud Support Engineer",
+      company_name: "Accenture",
+      icon: stadia,
+      iconBg: "#E6DEDD",
+      date: "Oct 2019 - Nov 2022",
+      points: [
+        "Upheld every facet of the Stadia service, acting as a mediary between engineers and game developers.",
+        "Shielded engineers from technical concerns raised by clients about our API and SDK escalations, with an average deflection rate of 85%",
+        "Established Stadia supports high reputation, maintaining a personal average Customer Satisfaction(CSAT) rating of 4.94/5 and a team average of 4.86/5 over three years.",
+        "Managed an evolving case queue by optimizing task assignments and prioritizing tickets for timely and effectivem resolution.",
+      ],
+    },
+    {
+      title: "UX Engineer",
+      company_name: "Beautycon Media",
+      icon: beautycon,
+      iconBg: "#383E56",
+      date: "Jun 2019 - August 2019",
+      points: [
+        "Built a mobile application interface for iOS and Android platforms on a 6-week timeline.",
+        "Designed a front-end application with Javascript and HTML/CSS",
+        "Implemented live conversion from a Wordpress web-app to the application framework.",
+        "Application was downloaded by over 2000 event attendees.",
+      ],
+    },
+    {
+      title: "Game Systems Engineer",
+      company_name: "Savor Saber",
+      icon: savorsaber,
+      iconBg: "#E6DEDD",
+      date: "September 2018 - June 2019",
+      points: [
+        "Built an award-winning 2D adventure JRPG, Savor Saber. Co-designed and implemented Utility based AI, Finite State Machines, and A* pathfinding in Unity with a team of 11.",
+        "Savor Saber won multiple awards at the UCSC Games Showcase and received a $5000 scholarship to continue development after graduation.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
   ];
   
   const projects = [
